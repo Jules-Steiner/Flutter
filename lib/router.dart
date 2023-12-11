1b56ca6a6a5d6ff31ce5bcfe88b91fc52c11b7e8
Jules-Steiner <97162125+Jules-Steiner@users.noreply.github.com>
2023-12-11 16:30:45 +0100

    creations arborescence

---
+++ lib/router.dart
@@ -0,0 +1,15 @@
+import 'package:projet_flutter_tram/ui.screens/departure _time.dart';
+import 'package:projet_flutter_tram/ui.screens/map.dart';
+import 'package:projet_flutter_tram/ui.screens/stops_list.dart';
+
+class AppRouter {
+  static const String mapPage = '/map';
+  static const String departureTimesPage = '/departure_times';
+  static const String stopsListPage = '/stops_list';
+
+  static final routes = {
+    mapPage: (context) =>  Map(),
+    departureTimesPage: (context) =>  DepartureTimes(),
+    stopsListPage: (context) =>  StopsList(),
+  };
+}
