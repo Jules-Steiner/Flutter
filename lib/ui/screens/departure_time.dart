f78284ed7e044a7f37764f8cc547b04d6605492d
Jules-Steiner <97162125+Jules-Steiner@users.noreply.github.com>
2024-01-08 14:00:31 +0100

    passer information entre map et departure time

--- lib/ui/screens/departure_time.dart
+++ lib/ui/screens/departure_time.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 class DepartureTime extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Récupérer les arguments passés depuis la page précédente
+    final Map<String, dynamic> args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    final String stopName = args['name'] as String; // Récupérer le nom de l'arrêt
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Map Page'),
       ),
       body: Center(
-        child: Text('Contenu de la page de la carte'),
+        child: Text('Arrêt sélectionné : $stopName'), // Afficher le nom de l'arrêt
       ),
     );
   }
--- lib/ui/screens/map.dart
+++ lib/ui/screens/map.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
@@ -12,23 +11,26 @@ class Map extends StatefulWidget {
 }
 
 class _MapState extends State<Map> {
-  int _selectedIndex = 0;
-
   void _onItemTapped(int index) {
     setState(() {
-      _selectedIndex = index;
+      // Votre logique pour le changement d'état ici
     });
   }
 
   void _onMarkerTap(BuildContext context, String name) {
-    Navigator.of(context).pushNamed(AppRouter.departureTimesPage);
-    // You can also pass data to DepartureTime screen if needed
-    // Navigator.pushNamed(context, '/departure_times', arguments: {'name': name});
+    Navigator.of(context).pushNamed(
+      AppRouter.departureTimesPage,
+      arguments: {'name': name},
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Carte'),
+        // Vous pouvez ajouter des actions, des icônes ou d'autres éléments à l'app bar ici
+      ),
       body: FlutterMap(
         options: MapOptions(
           center: LatLng(47.478419, -0.563166),
@@ -41,7 +43,7 @@ class _MapState extends State<Map> {
           MarkerLayerOptions(
             markers: [
               for (final location in [
-                {'name': 'Eseo', 'lat': 47.478419, 'lng': -0.563166, 'color': Colors.red},
+                {'name': 'Eseo', 'lat': 47.4937187, 'lng': -0.5504861, 'color': Colors.red},
                 {'name': 'Exemple 2', 'lat': 47.4725, 'lng': -0.5541, 'color': Colors.blue},
                 {'name': 'Exemple 3', 'lat': 47.4707, 'lng': -0.5534, 'color': Colors.green},
                 // Ajoutez autant d'emplacements que nécessaire avec leurs coordonnées et couleurs respectives
@@ -55,9 +57,9 @@ class _MapState extends State<Map> {
                       _onMarkerTap(context, location['name']! as String);
                     },
                     child: Container(
-                      width: 80.0, // Définir la largeur pour aligner les marqueurs horizontalement
+                      width: 80.0,
                       child: Stack(
-                        alignment: Alignment.center, // Aligner les éléments au centre horizontalement
+                        alignment: Alignment.center,
                         children: [
                           Icon(
                             Icons.location_pin,
